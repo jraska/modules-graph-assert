ae62bc7b966f5e2ddb8fb7f91cbefb50e2780866
Josef Raska <6277721+jraska@users.noreply.github.com>
2020-02-19 23:37:03 +0100

    Add Input annotation for assert graph task

--- plugin/src/main/kotlin/com/jraska/module/graph/assertion/tasks/AssertGraphTask.kt
+++ plugin/src/main/kotlin/com/jraska/module/graph/assertion/tasks/AssertGraphTask.kt
@@ -4,10 +4,12 @@ import com.jraska.module.graph.assertion.GradleDependencyGraphFactory
 import com.jraska.module.graph.assertion.GraphAssert
 import com.jraska.module.graph.assertion.UserDefinedRulesAssert
 import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.TaskAction
 
 open class AssertGraphTask : DefaultTask() {
 
+  @Input
   lateinit var assertion: GraphAssert
 
   @TaskAction
