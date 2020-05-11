be9063a2253c2c4b4ed7e97541427edea5c43bb6
Josef Raska <6277721+jraska@users.noreply.github.com>
2020-05-12 01:02:23 +0200

    Support group matches in regex

--- plugin/src/main/kotlin/com/jraska/module/graph/RegexpDependencyMatcher.kt
+++ plugin/src/main/kotlin/com/jraska/module/graph/RegexpDependencyMatcher.kt
@@ -1,17 +1,16 @@
 package com.jraska.module.graph
 
 class RegexpDependencyMatcher(
-  private val parentRegex: Regex,
-  private val childRegex: Regex,
+  private val matchingRegex: Regex,
   private val divider: String
 ) : DependencyMatcher {
 
   override fun matches(dependency: Pair<String, String>): Boolean {
-    return dependency.first.matches(parentRegex)
-      && dependency.second.matches(childRegex)
+    val dependencyToMatch = "${dependency.first}$divider${dependency.second}"
+    return matchingRegex.matches(dependencyToMatch)
   }
 
   override fun toString(): String {
-    return "$parentRegex$divider$childRegex"
+    return matchingRegex.toString()
   }
 }
--- plugin/src/test/kotlin/com/jraska/module/graph/ParseTest.kt
+++ plugin/src/test/kotlin/com/jraska/module/graph/ParseTest.kt
@@ -1,7 +1,6 @@
 package com.jraska.module.graph
 
 import org.junit.Test
-import java.lang.IllegalArgumentException
 
 class ParseTest {
   @Test
@@ -29,6 +28,29 @@ class ParseTest {
     assert(matcher.matches(":feature:aboutX" to ":lib"))
     assert(!matcher.matches(":feature:aboutX" to ":libx"))
     assert(!matcher.matches(":feature-about" to ":lib"))
+  }
+
+  @Test
+  fun groupsMatchingIsSupported() {
+    val groupingRegex = ":features:(\\S*):\\S* -> :features:\\1:\\S*"
+
+    val matcher = Parse.matcher(groupingRegex)
+
+    assert(matcher.matches(":features:X:Y" to ":features:X:Z"))
+    assert(matcher.matches(":features:data:Y" to ":features:data:Z"))
+    assert(!matcher.matches(":features:data:Y" to ":features:dat:Z"))
+  }
+
+  @Test
+  fun groupsMatchingIsSupportedForRestricted() {
+    val groupingRegex = ":features:(\\S*):(\\S*) -X> :features:\\1:\\2:\\1"
+
+    val restrictiveMatcher = Parse.restrictiveMatcher(groupingRegex)
+
+    assert(restrictiveMatcher.matches(":features:X:Y" to ":features:X:Y:X"))
+    assert(restrictiveMatcher.matches(":features:data:core" to ":features:data:core:data"))
+    assert(!restrictiveMatcher.matches(":features:X:Y" to ":features:X:Z"))
+    assert(!restrictiveMatcher.matches(":features:data:Y" to ":features:dat:Z"))
   }
 
   @Test(expected = IllegalArgumentException::class)
