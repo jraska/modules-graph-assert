49c7ea82ad1c68620cb59d969f3b1ba279a08c5d
Daniel Horowitz <daniel.horowitz@n26.com>
2022-03-24 15:30:01 +0100

    Remove experimental function to generate report file

--- plugin/src/main/kotlin/com/jraska/module/graph/assertion/report/ModuleGraphStatisticsReporter.kt
+++ plugin/src/main/kotlin/com/jraska/module/graph/assertion/report/ModuleGraphStatisticsReporter.kt
@@ -1,22 +1,18 @@
 package com.jraska.module.graph.assertion.report
 
 import com.jraska.module.graph.GraphStatistics
-import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.builtins.ListSerializer
 import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.encodeToStream
 import org.gradle.api.invocation.Gradle
 import java.io.File
 
-@OptIn(ExperimentalSerializationApi::class)
 object ModuleGraphStatisticsReporter {
   private const val REPORT_FILE = "reports/graph/statistics/report.json"
 
   fun report(gradle: Gradle, statistics: List<GraphStatistics>) {
     File(gradle.rootProject.buildDir, REPORT_FILE)
       .createDirsAndFile()
-      .outputStream()
-      .use { Json.encodeToStream(ListSerializer(GraphStatistics.serializer()), statistics, it) }
+      .writeText(Json.encodeToString(ListSerializer(GraphStatistics.serializer()), statistics))
   }
 
   private fun File.createDirsAndFile() = run {
