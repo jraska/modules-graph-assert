9185b096df1c240490140826e724ad1dcc0cf0aa
Blake Polidore <blake.polidore@instacart.com>
2024-10-16 13:00:42 -0500

    Allowed Violations

--- plugin/src/main/kotlin/com/jraska/module/graph/assertion/GraphRulesExtension.kt
+++ plugin/src/main/kotlin/com/jraska/module/graph/assertion/GraphRulesExtension.kt
@@ -4,6 +4,7 @@ open class GraphRulesExtension {
   var maxHeight: Int = 0
   var restricted = emptyArray<String>() // each restriction in format "regexp -X> regexp" e.g.: ":feature-[a-z]* -X> :forbidden-lib"
   var allowed = emptyArray<String>() // each allowance in format "regexp -> regexp" e.g.: ":feature-[a-z]* -> :forbidden-lib"
+  var allowedViolations = emptyMap<String, List<String>>() // each allowed violation in the format ["feature-[a-z]*" : [forbidden-lib1, forbidden-lib2]]
   var configurations: Set<String> = Api.API_IMPLEMENTATION_CONFIGURATIONS
   var assertOnAnyBuild: Boolean = false
 
